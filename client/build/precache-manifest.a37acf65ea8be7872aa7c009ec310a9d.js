369c95e31787b1105ae85c9408b8b4dd36395ba2
unknown <guillaume@savics.org>
2019-04-22 11:05:14 -0400

    Bug Fix: Build Files where ignored

---
+++ client/build/precache-manifest.a37acf65ea8be7872aa7c009ec310a9d.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "4ce137f900e09dd7d4d3",
+    "url": "/static/js/main.a5660b0e.chunk.js"
+  },
+  {
+    "revision": "4ab1c3f7e6816d119718",
+    "url": "/static/js/2.28e4e8d4.chunk.js"
+  },
+  {
+    "revision": "e69a7cd54ef39f758c8319467339a41b",
+    "url": "/index.html"
+  }
+];
