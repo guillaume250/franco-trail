412d8b5bff9ede222dd8ead6557cae47cecb5c4e
unknown <guillaume@savics.org>
2019-04-22 10:57:26 -0400

    pointed default api to client/build

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const path = require("path");
 
 app.get("/", function(req, res) {
-  res.sendFile(path.join(__dirname + "/client/public/index.html"));
+  res.sendFile(path.join(__dirname + "/client/build/index.html"));
 });
 
 const PORT = process.env.PORT || 5000;
